9b375f75ef1c81aee979251f53f0606a6de6c304
mr_kim <rofalfl42@gmail.com>
2022-07-22 22:01:03 +0900

    **프로젝트** : QR Generator **작성자** : illi **수정내역**: 1. Navigation graph 수정 2. BottomNavigationView 동작 추가

--- QRGenerator/app/src/main/java/com/illis/qrgenerator/MainActivity.kt
+++ QRGenerator/app/src/main/java/com/illis/qrgenerator/MainActivity.kt
@@ -2,10 +2,46 @@ package com.illis.qrgenerator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.google.android.material.navigation.NavigationBarView
+import com.illis.qrgenerator.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding.lifecycleOwner = this
+
+        supportFragmentManager.beginTransaction().add(R.id.page_bird, BirdFragment()).commit()
+        binding.bottomNavigationView.setOnItemSelectedListener { item ->
+            when(item.itemId) {
+                R.id.page_bird -> {
+                    supportFragmentManager
+                        .beginTransaction()
+                        .replace(R.id.page_bird , BirdFragment())
+                        .commitAllowingStateLoss()
+                    return@setOnItemSelectedListener true
+                }
+                R.id.page_cat -> {
+                    supportFragmentManager
+                        .beginTransaction()
+                        .replace(R.id.page_cat, CatFragment())
+                        .commitAllowingStateLoss()
+                    return@setOnItemSelectedListener true
+                }
+                R.id.page_dog -> {
+                    supportFragmentManager
+                        .beginTransaction()
+                        .replace(R.id.page_dog, DogFragment())
+                        .commitAllowingStateLoss()
+                    return@setOnItemSelectedListener true
+                }
+            }
+
+            return@setOnItemSelectedListener false
+        }
+
     }
 }
