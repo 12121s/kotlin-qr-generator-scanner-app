6754ecb322e2c6a9159a33eaf186f5d07bf7f6f6
mr_kim <rofalfl42@gmail.com>
2022-07-26 23:38:03 +0900

    **프로젝트** : QR Scanner **작성자** : illi **수정내역**: 1. qr 스캔 테스트 코드 추가

--- QRScanner/app/src/main/java/com/illis/qrscanner/MainActivity.kt
+++ QRScanner/app/src/main/java/com/illis/qrscanner/MainActivity.kt
@@ -3,6 +3,7 @@ package com.illis.qrscanner
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Toast
 import com.google.zxing.integration.android.IntentIntegrator
 
@@ -10,6 +11,17 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        runQRcodeReader()
+    }
+    fun runQRcodeReader() {
+        val integrator = IntentIntegrator(this)
+        integrator.setDesiredBarcodeFormats(IntentIntegrator.QR_CODE)
+        integrator.setPrompt("QR Scanner!")
+        integrator.setCameraId(0)
+        integrator.setBeepEnabled(true)
+        integrator.setBarcodeImageEnabled(true)
+        integrator.initiateScan()
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
