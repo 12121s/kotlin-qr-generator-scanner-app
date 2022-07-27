68e9239eeef32859b3ef09de7ca4e2f9e353cb7b
mr_kim <rofalfl42@gmail.com>
2022-07-27 23:27:39 +0900

    **프로젝트** : QR Scanner **작성자** : illi **수정내역**: 1. qr 스캔 화면 커스텀 코드 추가

--- QRScanner/app/src/main/java/com/illis/qrscanner/MainActivity.kt
+++ QRScanner/app/src/main/java/com/illis/qrscanner/MainActivity.kt
@@ -6,14 +6,26 @@ import android.os.Bundle
 import android.view.View
 import android.widget.Toast
 import com.google.zxing.integration.android.IntentIntegrator
+import com.journeyapps.barcodescanner.CaptureManager
+import com.journeyapps.barcodescanner.DecoratedBarcodeView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var manager : CaptureManager
+    private lateinit var barcodeView : DecoratedBarcodeView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        runQRcodeReader()
+        barcodeView = findViewById(R.id.qr_scanner)
+
+        manager = CaptureManager(this, barcodeView)
+        manager.initializeFromIntent(intent, savedInstanceState)
+        manager.decode()
+
+//        runQRcodeReader()
     }
+
     fun runQRcodeReader() {
         val integrator = IntentIntegrator(this)
         integrator.setDesiredBarcodeFormats(IntentIntegrator.QR_CODE)
@@ -24,11 +36,32 @@ class MainActivity : AppCompatActivity() {
         integrator.initiateScan()
     }
 
+    override fun onResume() {
+        super.onResume()
+        manager.onResume()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        manager.onPause()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        manager.onDestroy()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        manager.onSaveInstanceState(outState)
+    }
+
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         val qrCode = IntentIntegrator.parseActivityResult(requestCode, resultCode, data)
         if (qrCode != null) {
             if (qrCode.contents != null) {
-                Toast.makeText(this, "qrCode contents: ${qrCode.contents}", Toast.LENGTH_LONG).show()
+                Toast.makeText(this, "qrCode contents: ${qrCode.contents}", Toast.LENGTH_LONG)
+                    .show()
             }
         } else {
             super.onActivityResult(requestCode, resultCode, data)
