603b667a85f6777acfec380f530ee5379fac07cc
mr_kim <rofalfl42@gmail.com>
2022-08-02 23:12:53 +0900

    [작성자] 김미리 [개발 버전] - 서버 기반 변경 개발 (구) U+TV Remocon (현) Remocon [수정 항목] - Scanner gui (전체화면, 투명하게, 앵글 적용) - Scanner intent -> scanOption으로 동작 변경

--- QRScanner/app/src/main/java/com/illis/qrscanner/MainActivity.kt
+++ QRScanner/app/src/main/java/com/illis/qrscanner/MainActivity.kt
@@ -1,13 +1,14 @@
 package com.illis.qrscanner
 
-import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.google.zxing.client.android.Intents
 import com.google.zxing.integration.android.IntentIntegrator
 import com.journeyapps.barcodescanner.CaptureManager
 import com.journeyapps.barcodescanner.DecoratedBarcodeView
+import com.journeyapps.barcodescanner.ScanContract
+import com.journeyapps.barcodescanner.ScanOptions
 
 class MainActivity : AppCompatActivity() {
     private lateinit var manager : CaptureManager
@@ -24,16 +25,6 @@ class MainActivity : AppCompatActivity() {
         manager.decode()
 
 //        runQRcodeReader()
-    }
-
-    fun runQRcodeReader() {
-        val integrator = IntentIntegrator(this)
-        integrator.setDesiredBarcodeFormats(IntentIntegrator.QR_CODE)
-        integrator.setPrompt("QR Scanner!")
-        integrator.setCameraId(0)
-        integrator.setBeepEnabled(true)
-        integrator.setBarcodeImageEnabled(true)
-        integrator.initiateScan()
     }
 
     override fun onResume() {
@@ -56,15 +47,25 @@ class MainActivity : AppCompatActivity() {
         manager.onSaveInstanceState(outState)
     }
 
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        val qrCode = IntentIntegrator.parseActivityResult(requestCode, resultCode, data)
-        if (qrCode != null) {
-            if (qrCode.contents != null) {
-                Toast.makeText(this, "qrCode contents: ${qrCode.contents}", Toast.LENGTH_LONG)
-                    .show()
+    private fun runQRcodeReader() {
+        val scanOptions = ScanOptions().setOrientationLocked(true)
+        scanOptions.setBarcodeImageEnabled(false)
+        scanOptions.setBeepEnabled(false)
+        scanOptions.captureActivity = MainActivity::class.java
+        barcodeLauncher.launch(scanOptions)
+    }
+
+    private val barcodeLauncher = registerForActivityResult(ScanContract()) {
+            result ->
+        if(result.contents == null) {
+            val originalIntent = result.originalIntent
+            if (originalIntent == null) {
+                Toast.makeText(this, "Cancelled", Toast.LENGTH_LONG).show()
+            } else if(originalIntent.hasExtra(Intents.Scan.MISSING_CAMERA_PERMISSION)) {
+                Toast.makeText(this, "Cancelled due to missing camera permission", Toast.LENGTH_LONG).show()
             }
         } else {
-            super.onActivityResult(requestCode, resultCode, data)
+            Toast.makeText(this, "Scanned: ${result.contents}", Toast.LENGTH_LONG).show()
         }
     }
 }
